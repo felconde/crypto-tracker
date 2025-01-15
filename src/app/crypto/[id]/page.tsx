8ebfd3e7e7ae656ea97192ea7bc43d95414214f8
felipecondeluiz <felconde@gmail.com>
2025-01-15 13:49:42 -0300

    fix: dynamic routes

--- src/app/crypto/[id]/page.tsx
+++ src/app/crypto/[id]/page.tsx
@@ -1,32 +1,28 @@
-import axios from "axios";
-import { Crypto } from "@/types/crypto";
-import { Are_You_Serious } from "next/font/google";
+import axios from 'axios';
+import { Crypto } from '../../../types/crypto';
 
-const fetchCryptoDetais = async (id: string): Promise<Crypto> => {
-    const res = await axios.get(`https://api.coingecko.com/api/v3/coins/${id}`);    
-    return res.data;
+// Define o formato correto dos parâmetros
+interface PageProps {
+  params: {
+    id: string;
+  };
 }
 
-export async function generateStaticParams() {
+// Função para buscar os detalhes de uma criptomoeda
+const fetchCryptoDetails = async (id: string): Promise<Crypto> => {
+  const res = await axios.get(`https://api.coingecko.com/api/v3/coins/${id}`);
+  return res.data;
+};
 
-    const res = await axios.get(`https://api.coingecko.com/api/v3/coins/markets`, {
-        params: { vs_currency: 'usd', order: 'market_cap_desc', per_page: 10 },
-    });
+// Página de detalhes da criptomoeda
+export default async function CryptoDetails({ params }: any) {
+  const crypto = await fetchCryptoDetails(params.id);
 
-    return res.data.map((crypto: Crypto) => ({
-        id: crypto.id,
-    }));
-    
-}
-
-export default async function CryptoDetails({params}:{params: {id: string}}) {
-    const crypto = await fetchCryptoDetais(params.id);
-
-    return (
-        <div>
-            <h1>{crypto.name}</h1>
-            <p>{crypto.description?.en || 'No description available'}</p>
-            <p>Current Price: ${crypto.current_price}</p>
-        </div>
-    );
+  return (
+    <div>
+      <h1>{crypto.name}</h1>
+      <p>{crypto.description?.en || 'No description available.'}</p>
+      <p>Current Price: ${crypto.current_price}</p>
+    </div>
+  );
 }
